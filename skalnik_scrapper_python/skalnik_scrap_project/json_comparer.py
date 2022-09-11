39521dfc2a3fc99dc10c28fc2869893de1d0cf21
fjed <d.ostapczuk1@gmail.com>
2022-09-11 23:20:35 +0200

    add todo, modify structure

--- skalnik_scrapper_python/skalnik_scrap_project/json_comparer.py
+++ skalnik_scrapper_python/skalnik_scrap_project/json_comparer.py
@@ -1,10 +1,12 @@
+import sys
 from deepdiff import DeepDiff
 
-file1 = open('ekspresy.json' ,'r')
-file2 = open('ekspresy_11_09_2022.json', 'r')
+def comparer():
+    # pass two files as args, todo: automatic pick two previous json files and compare them
+    json_today = sys.argv[0]
+    json_yesterday = sys.argv[1]
+    #figure out this function 
+    difference = DeepDiff(open(json_today, 'r'), open(json_yesterday, 'r'))
 
-difference = DeepDiff(file1, file2)
-
-
-
-print(f" ITEMS ADDED: {difference['values_changed']}\n")
+    print(difference)
+comparer()
