8b8691a85528f7adbca4ba4267b4cf23bdc2f556
fjed <d.ostapczuk1@gmail.com>
2022-09-15 21:33:38 +0200

    created json_comparer.py: showing difference between 2 latest json files

--- skalnik_scrapper_python/skalnik_scrap_project/json_comparer.py
+++ skalnik_scrapper_python/skalnik_scrap_project/json_comparer.py
@@ -1,12 +1,20 @@
-import sys
-from deepdiff import DeepDiff
+from importlib.resources import path
+import glob
+from itertools import product
+import os, glob
 
 def comparer():
-    # pass two files as args, todo: automatic pick two previous json files and compare them
-    json_today = sys.argv[0]
-    json_yesterday = sys.argv[1]
-    #figure out this function 
-    difference = DeepDiff(open(json_today, 'r'), open(json_yesterday, 'r'))
+    # get list of files in json_store dir - change path
+    json_list = glob.glob(#/YOUR PATH/json_store)
+    # get 2 latest json files to compare products 
+    json_latest = sorted(json_list, key = os.path.getctime)[-1]
+    json_2nd_latest = sorted(json_list, key = os.path.getctime)[-2]
+    
+    # check which products changed since last crawl
+    with open(json_latest ,'r') as json1, open(json_2nd_latest, 'r') as json2:
+        difference = set(json2) - set(json1) 
 
-    print(difference)
+    # print products that changed
+    print(f'Products that changed since yesterday ({len(difference)}):')
+    print('\n'.join(product for product in difference))
 comparer()
